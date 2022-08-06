1ecda36ed41f8ac86a91364c32d7bc62bbe3ccb2
RythBiss <dkschaarschmidt@gmail.com>
2022-08-06 09:24:58 -0400

    Generate Hexagon Shape
    Copied some CSS from online to create a hexagon. Will be used to create a grid for the splash page with subtle hover effect.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import './App.css';
 import spaceBlob from './img/Space Blob.gif'
 import MajorSegment from './MajorSegment.js'
 
-const spaceBlobElement = <>
-  <div>
-    <div className='splashText'></div>
-    <img id='spaceBlob' src={spaceBlob} alt='spaceBlob' />
-    <div className='splashText'></div>
+const spaceBlobElement =
+  <div  id='spaceBlob'>
+    <div className='splashTextCont'>
+      <h1 className='textLight'>Davd Schaarachmidt</h1>
+      <div id='line'></div>
+      <h1 className='textLight'>Frontend Developer</h1>
+    </div>
+    <img id='img' src={spaceBlob} alt='spaceBlob' />
   </div>
-</>
+
+  const hexBackgroundElement = <div className='hexagon'/>;
 
 function App() {
   return (
     <div className="App">
       <MajorSegment body={spaceBlobElement} styling='majorSegLight' />
-      <MajorSegment body='List skills here' styling='majorSegDark' />
+      <MajorSegment body={hexBackgroundElement} styling='majorSegDark' />
       <MajorSegment body='Add links to projects' styling='majorSegLight' />
     </div>
   );
