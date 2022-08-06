3a950357056cf51c517585ea112c613fba66c8b0
RythBiss <dkschaarschmidt@gmail.com>
2022-08-05 22:58:32 -0400

    Splash screen text
    Began adding text to the splash screen. Styling not complete.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 import spaceBlob from './img/Space Blob.gif'
 import MajorSegment from './MajorSegment.js'
 
-const spaceBlobElement = <img id='spaceBlob' src={spaceBlob} alt='spaceBlob' />
+const spaceBlobElement = <>
+  <div>
+    <div className='splashText'></div>
+    <img id='spaceBlob' src={spaceBlob} alt='spaceBlob' />
+    <div className='splashText'></div>
+  </div>
+</>
 
 function App() {
   return (
