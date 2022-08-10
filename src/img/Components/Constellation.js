c4f2d24927e9eea113c94faa30ba0804b35dabed
RythBiss <dkschaarschmidt@gmail.com>
2022-08-09 22:14:25 -0400

    CSS:hover animation
    Constellation animates on hover.

--- src/img/Components/Constellation.js
+++ src/img/Components/Constellation.js
@@ -4,7 +4,7 @@ const Orion =
 
 <svg id='stars' width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M259 461L222.5 270.5L100.5 132.5L171 51L259 75.5L286.5 239L411.5 403M283.5 240.5L254 255L224 269.5" stroke="#FFFFF2" stroke-width="3"/>
-        <g id='stars'>
+        <g id='dots'>
             <circle cx="412" cy="404" r="8" fill="#FFFFF2"/>
             <circle cx="260" cy="465" r="8" fill="#FFFFF2"/>
             <circle cx="223" cy="268" r="8" fill="#FFFFF2"/>
