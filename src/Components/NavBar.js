27e1f5173334efea6607ea02a732808bcdeedd92
RythBiss <dkschaarschmidt@gmail.com>
2022-09-07 10:53:34 -0400

    CSS Tweaks
    Small CSS tweaks

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -11,7 +11,7 @@ const handleClick = (ref) => {
           <li onClick={() => handleClick(props.refArray[0])} className='navButton' >Home</li>
           <li onClick={() => handleClick(props.refArray[1])} className='navButton' >About</li>
           <li onClick={() => handleClick(props.refArray[2])} className='navButton' >Skills</li>
-          <li onClick={() => handleClick(props.refArray[3])} className='navButton' >Work</li>
+          <li onClick={() => handleClick(props.refArray[3])} className='navButton' >Projects</li>
           <li onClick={() => handleClick(props.refArray[4])} className='navButton' >Contact</li>
         </ul>
   )
